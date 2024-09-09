6ffa20bc919f155a27856b4337e467bec1719db5
Mohau Mushi <david1mushi1@gmail.com>
2024-09-09 09:18:02 +0200

    Updated JSX syntax to use className instead of class, also added priority to image

--- components/LoadingSpinner.jsx
+++ components/LoadingSpinner.jsx
@@ -1,10 +1,10 @@
 export default function LoadingSpinner() {
   return (
-    <div class="flex space-x-2 justify-center items-center h-screen dark:invert">
-      <span class="sr-only">Loading...</span>
-      <div class="h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-      <div class="h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-      <div class="h-5 w-5 bg-black rounded-full animate-bounce"></div>
+    <div className="flex space-x-2 justify-center items-center h-screen dark:invert">
+      <span className="sr-only">Loading...</span>
+      <div className="h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div className="h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+      <div className="h-5 w-5 bg-black rounded-full animate-bounce"></div>
     </div>
   );
 }
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -12,10 +12,11 @@ export default function ProductCard({ product }) {
               alt={product.title}
               width={300}
               height={200}
+              priority={true}
               className="w-full mt-5 h-48 object-contain"
             />
             <div className="p-4">
-              <h2 class="text-lg font-semibold text-gray-00 mb-2 line-clamp-2">
+              <h2 className="text-lg font-semibold text-gray-00 mb-2 line-clamp-2">
                 {product.title}
               </h2>
               <div className="flex justify-between items-center">
@@ -23,8 +24,8 @@ export default function ProductCard({ product }) {
                   ${product.price.toFixed(2)}
                 </p>
               </div>
-              <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-2 space-y-2 sm:space-y-0">
-                <p class="px-2 py-1 bg-indigo-100 text-[#415a77] rounded-md text-xs font-medium">
+              <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-2 space-y-2 sm:space-y-0">
+                <p className="px-2 py-1 bg-indigo-100 text-[#415a77] rounded-md text-xs font-medium">
                   {product.category}
                 </p>
               </div>
