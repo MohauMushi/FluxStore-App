50925db8e8787e4a301c4cb74d5978c05eb280b1
Mohau Mushi <david1mushi1@gmail.com>
2024-09-09 10:16:55 +0200

    Updated Home page styling

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import ProductGrid from "../components/ProductGrid";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-8">
+    <main className="min-h-screen px-4 py-8 bg-gray-100 text-gray-900 transition-colors duration-300">
       <h1 className="text-3xl font-bold mb-8">Products</h1>
       <ProductGrid />
     </main>
