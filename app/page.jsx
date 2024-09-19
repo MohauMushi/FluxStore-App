b828219fbe28014a84e5c6af9dce2b55f7b7daa2
Mohau Mushi <david1mushi1@gmail.com>
2024-09-19 16:24:15 +0200

    Removed SearchBar, CategoryFilter, and PriceSort components from Page component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,6 @@
 import ProductGrid from "../components/ProductGrid";
 import { Suspense } from "react";
 import LoadingSpinner from "../components/LoadingSpinner";
-import SearchBar from "../components/SearchBar";
-import CategoryFilter from "../components/CategoryFilter";
-import PriceSort from "../components/PriceSort";
 
 export const metadata = {
   title: "FluxStore - Shop the Latest Products",
@@ -18,17 +15,6 @@ export default function Home() {
   return (
     // Main content area with styling for minimum height, padding, and background
     <main className="min-h-screen px-4 bg-gray-100 text-gray-900 transition-colors duration-300">
-      <div className="mt-6 flex flex-col sm:flex-row items-center justify-between gap-4">
-        <div className="w-full sm:w-auto">
-          <CategoryFilter />
-        </div>
-        {/* <div className="w-full sm:w-auto max-w-md">
-          <SearchBar />
-        </div> */}
-        <div className="w-full sm:w-auto">
-          <PriceSort />
-        </div>
-      </div>
       <div className="container mx-auto py-8">
         <Suspense fallback={<LoadingSpinner />}>
           <ProductGrid />
