91cbcefcd9d06642b1ca48a12d14fafe55864582
Mohau Mushi <david1mushi1@gmail.com>
2024-10-01 19:45:56 +0200

    Updated charset attribute in meta tag to use camelCase

--- app/layout.jsx
+++ app/layout.jsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <link rel="icon" href="/Accusoft/favicon.ico" sizes="any" />
         <link
           rel="apple-touch-icon"
