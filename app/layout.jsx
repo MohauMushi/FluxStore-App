2014e4ff28301c3d707a74594e0d9b4ef26fe3df
Mohau Mushi <david1mushi1@gmail.com>
2024-10-01 11:02:08 +0200

    Added head section with metadata and favicon links to RootLayout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/Accusoft/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/Accusoft/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/Accusoft/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/Accusoft/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/Accusoft/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>FlexStore</title>
+      </head>
       <body>
         {/* Navigation bar component */}
         <Navbar />
