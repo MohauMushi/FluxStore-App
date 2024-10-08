6b582a4c378bd113a0d52bcc0e82bfe93bc7e638
Mohau Mushi <david1mushi1@gmail.com>
2024-10-08 23:18:19 +0200

    Configured next-pwa in my next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from 'next-pwa'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -10,6 +12,14 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+export default pwaConfig(nextConfig)
+
