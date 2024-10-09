cbe6605dee749de6167edc1b18490702abafcd53
Mohau Mushi <david1mushi1@gmail.com>
2024-10-09 13:37:31 +0200

    Updated the configuration of Next.js with next-pwa plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import withPWA from 'next-pwa'
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,15 +11,69 @@ const nextConfig = {
         pathname: "/**",
       },
     ],
+    domains: ["firebasestorage.googleapis.com"],
   },
-}
+};
 
 const pwaConfig = withPWA({
-  dest: 'public',
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/firebasestorage\.googleapis\.com/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "firebase-images",
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/_next\/image\?url/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "next-image",
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/_next\/static\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-resources",
+      },
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-font-assets",
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/api\/.*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "apis",
+        expiration: {
+          maxEntries: 16,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+        networkTimeoutSeconds: 10,
+      },
+    },
+  ],
+});
 
-export default pwaConfig(nextConfig)
-
+export default pwaConfig(nextConfig);
